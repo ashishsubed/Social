67d7df67863208871fdf9ded8452ef1d1ce4a37f
yukes <yukesh_mynepal@yahoo.com>
2018-12-19 12:23:50 -0500

    Merge remote-tracking branch 'remotes/origin/test' into testing
    # Conflicts:
    #	app/src/main/java/com/example/ashis/social/MainActivity.java

--- app/src/main/java/com/example/ashis/social/MainActivity.java
+++ app/src/main/java/com/example/ashis/social/MainActivity.java
@@ -9,11 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //test branch
-        //changes
-        // changing in master
-        //testing again
-        //abcd
-        //xyz
     }
 }
